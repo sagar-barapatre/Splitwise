c144f0d60c741fafd60d90f146c9f526fd5852f5
Sagar Barapatre <b119016@iiit-bh.ac.in>
2021-02-23 06:14:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config');
+const timer = require('./server/timer/timer');
+
+// connect to the database and load models
+let db = process.env.MONGODB_URI || config.dbUri;
+require('./server/models').connect(db);
+
+const app = express();
+// tell the app to look for static files in these directories
+app.use(express.static('./server/static/'));
+app.use(express.static('./client/dist/'));
+// tell the app to parse HTTP body messages
+app.use(bodyParser.urlencoded({ extended: false }));
+// pass the passport middleware
+app.use(passport.initialize());
+
+// load passport strategies
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authenticaion checker middleware
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
+
+// routes
+const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
+
+// bill dates check timer
+setInterval(timer, 86400000);
+
+// PORT
+let PORT = process.env.PORT || 3000;
+
+// start the server
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
+});
